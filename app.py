57d013fdce5a8bd2d6f5380bdde31eeb4a1638bf
Moonzi <2505840201@qq.com>
2025-12-04 22:42:26 +0800

    准备部署到Vercel

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify
 import requests
 import json
@@ -98,4 +99,5 @@ def generate():
         return jsonify({"success": False, "error": str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get("PORT",5001))
+    app.run(host='0.0.0.0',port=port,debug=False)
